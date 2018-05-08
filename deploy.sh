ded8a99e142cbd8b0f953ad97180b9395b4018fb
LamhotJM <lamhotjms@gmail.com>
2018-05-08 15:31:56 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,20 @@ case "$1" in
     ;;
   run-all)
    kill $(lsof -t -i:6643)
+   kill $(lsof -t -i:6622)
+   kill $(lsof -t -i:6619)
    kill $(lsof -t -i:8089)
    cd splitter
    exec ./splitter &
    cd ..
    cd aleppo
    exec ./aleppo &
+   cd ..
+   cd now-you-see-me
+   exec deploy/_output/api/bin/api app/api/main.go; &
+   cd ..
+   cd chronos
+   exec deploy/_output/api/bin/api &
     ;;
   stop)
     kill $(lsof -t -i:6643)
