a14bf3acab7b0416bcfa4a3b9cffb9828ec2de7d
LamhotJM <lamhotjms@gmail.com>
2018-05-08 13:34:09 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,16 @@
 #!bin/bash
-ps axf | grep splitter | grep -v grep | awk '{print "kill -9 " $1}' | sh;
-dep ensure;
-make build;
-exec ./splitter &
+case "$1" in
+  start)
+    ps axf | grep splitter | grep -v grep | awk '{print "kill -9 " $1}' | sh;
+    dep ensure;
+    make build;
+    exec ./splitter &
     ;;
-stop
+  stop)
+    kill $(lsof -t -i:6643)
+    ;;
+  *)
+    echo $"Usage: $0 {start|stop}"
+    exit 1
+esac
+exit 0
